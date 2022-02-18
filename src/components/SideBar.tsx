1e9c108313e9637fa60b58e1843d56050be092bf
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-02-17 22:28:50 -0300

    create Sidebar component and functionalities

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,24 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+interface GenreItemProps {
+  genre: {
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  };
+  handleClickButton(id: number): any;
+  selectedGenreId: number;
+}
+
+export function SideBar(props: GenreItemProps) {
   // Complete aqui
+  return (
+    <Button
+      key={String(props.genre.id)}
+      title={props.genre.title}
+      iconName={props.genre.name}
+      onClick={() => props.handleClickButton(props.genre.id)}
+      selected={props.selectedGenreId === props.genre.id}
+    />
+  );
 }
